3a3a30525acf554a62e361386db5c0a0ec548bdf
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-09-11 14:01:56 +0530

    Protect Profile Route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Profile from "./components/Profile/Profile";
 import SocialCards from "./components/SocialCards/SocialCards";
+import { AUTH_TOKEN } from "./constant";
 
 const AppRoutes = () => {
+  const token = localStorage.getItem(AUTH_TOKEN);
   return (
     <Routes>
       <Route path="/" element={<SocialCards />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route
+        path="/profile"
+        element={token ? <Profile /> : <Navigate to="/" />}
+      />
     </Routes>
   );
 };
