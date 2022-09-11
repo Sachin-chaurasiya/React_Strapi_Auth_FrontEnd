63272ea62679e8687cfc2fd6b23afd283ce7cbf8
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-09-11 15:09:30 +0530

    Add register functionality

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,16 +4,18 @@ import Profile from "./components/Profile/Profile";
 import SocialCards from "./components/SocialCards/SocialCards";
 import { getToken } from "./helpers";
 import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<SocialCards />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route
         path="/profile"
         element={getToken() ? <Profile /> : <Navigate to="/signin" />}
       />
-      <Route path="/signin" element={getToken() ? <Profile /> : <SignIn />} />
     </Routes>
   );
 };
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,137 @@
+import {
+  Alert,
+  Button,
+  Card,
+  Col,
+  Form,
+  Input,
+  message,
+  Row,
+  Spin,
+  Typography,
+} from "antd";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../context/AuthContext";
+import useScreenSize from "../../hooks/useScreenSize";
+import { API } from "../../constant";
+import { setToken } from "../../helpers";
+
+const SignUp = () => {
+  const { isDesktopView } = useScreenSize();
+  const navigate = useNavigate();
+
+  const { setUser } = useAuthContext();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [error, setError] = useState("");
+
+  const onFinish = async (values) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${API}/auth/local/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+
+      const data = await response.json();
+      if (data?.error) {
+        throw data?.error;
+      } else {
+        // set the token
+        setToken(data.jwt);
+
+        // set the user
+        setUser(data.user);
+
+        message.success(`Welcome to Social Cards ${data.user.username}!`);
+
+        navigate("/profile");
+      }
+    } catch (error) {
+      console.error(error);
+      setError(error?.message ?? "Something went wrong!");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Fragment>
+      <Row align="middle">
+        <Col span={isDesktopView ? 8 : 24} offset={isDesktopView ? 8 : 0}>
+          <Card title="SignUp">
+            {error ? (
+              <Alert
+                className="alert_error"
+                message={error}
+                type="error"
+                closable
+                afterClose={() => setError("")}
+              />
+            ) : null}
+            <Form
+              name="basic"
+              layout="vertical"
+              onFinish={onFinish}
+              autoComplete="off"
+            >
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    type: "string",
+                  },
+                ]}
+              >
+                <Input placeholder="Username" />
+              </Form.Item>
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    type: "email",
+                  },
+                ]}
+              >
+                <Input placeholder="Email address" />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true }]}
+              >
+                <Input.Password placeholder="Password" />
+              </Form.Item>
+
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login_submit_btn"
+                >
+                  Submit {isLoading && <Spin size="small" />}
+                </Button>
+              </Form.Item>
+            </Form>
+            <Typography.Paragraph className="form_help_text">
+              Already have an account? <Link to="/signin">Sign In</Link>
+            </Typography.Paragraph>
+          </Card>
+        </Col>
+      </Row>
+    </Fragment>
+  );
+};
+
+export default SignUp;
